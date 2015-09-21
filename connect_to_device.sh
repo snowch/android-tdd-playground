feb061662952f0df50ecf1d74a507f36f6c10722
christopher snow <chsnow123@gmail.com>
2015-09-21 17:35:39 +0100

    try to manually install and run apk

--- connect_to_device.sh
+++ connect_to_device.sh
@@ -4,6 +4,10 @@ set -x
 
 find /home/travis/build/ -name "*.apk"
 
+adb install /home/travis/build/snowch/android-tdd-playground/build/apk/android-tdd-playground-debug-unaligned.apk
+
+adb shell am start -a android.intent.action.MAIN -n org.pestrada.android_tdd_playground/.MainActivity
+
 adb shell netstat -nalt
 
 curl -v http://$(adb shell netcfg | grep 'eth0' | awk '{ print $3 }' | cut -d'/' -f1):8182
